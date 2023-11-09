13df702b851e87e28af4465721e609f43eab6012
Sameer Sannidhanam <110780161+sameersannidhanam@users.noreply.github.com>
2023-11-09 22:29:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Color_Picker from "./components/Color_Picker";
+
+function App() {
+  return (
+    <div>
+      <Color_Picker />
+    </div>
+  );
+}
+
+export default App;
